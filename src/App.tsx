35aa6fa86a390ce1a2b7878404272545b9294eb5
ibnu_jemil <furkanjemil54@gmail.com>
2026-02-14 10:07:15 +0300

    feat(app): update main App configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import AppRoutes from '@/routes'
-import { ToastContainer } from '@/components/ui'
+import { ToastContainer, ErrorBoundary } from '@/components/ui'
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,11 +17,13 @@ const queryClient = new QueryClient({
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <AppRoutes />
-            </BrowserRouter>
-            <ToastContainer />
-            <ReactQueryDevtools initialIsOpen={false} />
+            <ErrorBoundary>
+                <BrowserRouter>
+                    <AppRoutes />
+                </BrowserRouter>
+                <ToastContainer />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </ErrorBoundary>
         </QueryClientProvider>
     )
 }
